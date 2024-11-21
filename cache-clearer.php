1ccd1521acdc2df05f24064c979f3dca379050df
gioshwua <reanokennethjoshua11@gmail.com>
2024-11-21 16:17:39 +0800

    Initialization of the project

---
+++ cache-clearer.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Staging Cache Clearer
+Plugin URI: https://www.linkedin.com/in/gioshwua/
+Description: A plugin to clear browser cache with a single click.
+Version: 1.0
+Author: Kenneth Joshua Reaño
+Author URI: https://www.linkedin.com/in/gioshwua/
+License: GPL2
+License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
